c0d6000243318aa072d4a384402dcdf292d85a23
Nathan Zimmerman <nate.m.zimmerman@gmail.com>
2020-09-25 21:32:47 -0500

    package update prep

--- package.js
+++
@@ -1,7 +0,0 @@
-
-
-//require('./Matrixs/matrix');
-//require('./Solvers/solvers');
-//require('./Models/models');
-Plots = require('./Plots/plots2');
-Matrix = require('./src/matrix_lib');
---
+++ src/package.js
@@ -0,0 +1,7 @@
+
+
+//require('./Matrixs/matrix');
+//require('./Solvers/solvers');
+//require('./Models/models');
+Plots = require('./Plots/plots2');
+Matrix = require('./src/matrix_lib');
